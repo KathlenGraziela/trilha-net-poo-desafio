90ee9af2fea3f22e7d3371a1bc79e0346f98e0ac
Kathlen Graziela <kathlengraziela@gmail.com>
2022-09-19 22:05:30 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new("47123448620", "Iphone14", "123456789", 520);
+iphone.Ligar();
+iphone.InstalarAplicativo("Dio");
+
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new("47984569610", "Nokia14", "456789123", 128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Dio");
